935d18ce868a96e0838da7a5747bda5b92df32b4
leonardocaag <leonardocaag@gmail.com>
2021-07-17 20:35:55 -0300

    Aula 4

--- pages/index.js
+++ pages/index.js
@@ -46,16 +46,14 @@ function ProfileRelationsBox(propriedades) {
 
 export default function Home() {
   const usuarioAleatorio = 'leonardocaag';
-  const [comunidades, setComunidades] = React.useState([{
-    id: '12802378123789378912789789123896123', 
-    title: 'Eu odeio acordar cedo',
+  const [comunidades, setComunidades] = React.useState([]);
+  //  id: '12802378123789378912789789123896123', 
+  //  title: 'Eu odeio acordar cedo',
   
-    image: 'https://alurakut.vercel.app/capa-comunidade-01.jpg'
-  }]);
+  //  image: 'https://alurakut.vercel.app/capa-comunidade-01.jpg'
+  
   // const comunidades = comunidades[0];
   // const alteradorDeComunidades/setComunidades = comunidades[1];
-
-  console.log('Nosso teste', );
   // const comunidades = ['Alurakut'];
   const pessoasFavoritas = [
     'juunegreiros',
@@ -68,13 +66,38 @@ export default function Home() {
 const [seguidores, setSeguidores] = React.useState([]);
   // 0 - Pegar o array de dados do github
   React.useEffect(function() {
-    fetch('https://api.github.com/user/leonardocaag/followers')
+    fetch('https://api.github.com/users/leonardocaag/followers')
     .then(function (respostaDoServidor) {
       return respostaDoServidor.json()
     })
     .then(function(respostaCompleta) {
       setSeguidores(respostaCompleta);
     })
+
+   // API Graph
+   fetch('https://graphql.datocms.com/' , {
+     method: 'POST',
+     headers: {
+       'Authorization': '170621f6eeca135cd63a1d3702af39',
+       'Content-Type': 'application/json',
+       'Accept': 'application/json'
+     },
+     body: JSON.stringify({ "query" : `query {
+      allCommunities {
+        title
+        id
+        imageUrl
+        creatorSlug
+      }
+    }` })
+   })
+   .then((response) => response.json()) // Pega o retorno do response.json() e já retorna
+   .then((respostaCompleta) => {
+     const comunidadesVindasDoDato = respostaCompleta.data.allCommunities
+     console.log(comunidadesVindasDoDato)
+     setComunidades(comunidadesVindasDoDato)
+
+   })
     
   }, [])
 
@@ -110,12 +133,25 @@ const [seguidores, setSeguidores] = React.useState([]);
                 console.log('Campo: ', dadosDoForm.get('image'));
 
                 const comunidade = {
-                  id: new Date().toISOString(),
                   title: dadosDoForm.get('title'),
-                  image: dadosDoForm.get('image'),
+                  imageUrl: dadosDoForm.get('image'),
+                  creatorSlug: usuarioAleatorio,
                 }
-                const comunidadesAtualizadas = [...comunidades, comunidade];
-                setComunidades(comunidadesAtualizadas)
+                
+                fetch('/api/comunidades', {
+                  method: 'POST',
+                  headers: {
+                    'Content-Type': 'application/json',
+                  },
+                  body: JSON.stringify(comunidade)
+                })
+                .then(async (response) => {
+                  const dados = await response.json();
+                  console.log(dados.registroCriado);
+                  const comunidade = dados.registroCriado;
+                  const comunidadesAtualizadas = [...comunidades, comunidade];
+                  setComunidades(comunidadesAtualizadas)
+                })
             }}>
               <div>
                 <input
@@ -149,8 +185,8 @@ const [seguidores, setSeguidores] = React.useState([]);
               {comunidades.map((itemAtual) => {
                 return (
                   <li key={itemAtual.id}>
-                    <a href={`/users/${itemAtual.title}`}>
-                      <img src={itemAtual.image} />
+                    <a href={`/comunidades/${itemAtual.id}`}>
+                      <img src={itemAtual.imageUrl} />
                       <span>{itemAtual.title}</span>
                     </a>
                   </li>
